242d10ba881136a6273af5c204d965976de63e3a
difosal <xande.genka@gmail.com>
2015-03-25 18:44:26 -0300

    Adicionado cálculos para critico

--- src/main/java/br/com/lolcalc/calc/DamageCalc.java
+++ src/main/java/br/com/lolcalc/calc/DamageCalc.java
@@ -10,10 +10,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/calc")
-public class DamageCalcImpl{
+public class DamageCalc{
 
 	@RequestMapping(value = "damage", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody String calcDamage(@RequestParam("damage") Integer damage, @RequestParam("armor") Integer armor, @RequestParam("penetration") Integer penetration) {
+	public @ResponseBody Integer calcDamage(@RequestParam("damage") Integer damage, @RequestParam("armor") Integer armor, @RequestParam("penetration") Integer penetration) {
 		Integer realDamage = null;
 		Integer armorTmp = armor - penetration;
 		if (armorTmp > 0) {
@@ -21,15 +21,25 @@ public class DamageCalcImpl{
 		} else {
 			realDamage = damage;
 		}
-		return String.valueOf(realDamage);
+		return realDamage;
 	}
+	
+	@RequestMapping(value = "calcDamagePerSeconds", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
+	public @ResponseBody Integer calcDamagePerSeconds(@RequestParam("damage") Integer damage, @RequestParam("armor") Integer armor, @RequestParam("penetration") Integer penetration, Double atackSpeed, Integer seconds) {
+		Integer totalAtacks = (int) Math.floor(atackSpeed * seconds);
+		Integer damagePerHit = calcDamage(damage, armor, penetration);
+		return totalAtacks * damagePerHit;
+	}
+	
 	@RequestMapping(value = "criticalStrikeByHits", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
-	public @ResponseBody String calcCriticalStrikeByHits(@RequestParam("damage") Integer damage, @RequestParam("armor") Integer armor, @RequestParam("penetration") Integer penetration, @RequestParam("critPercent") Double critPercent, @RequestParam("quanittyOfHits") Integer quanittyOfHits){
-		return null;
+	public @ResponseBody Integer calcHitsWithCriticalStrike(@RequestParam("damage") Integer damage, @RequestParam("armor") Integer armor, @RequestParam("penetration") Integer penetration, @RequestParam("critPercent") Double critPercent){
+		Double crit = new Integer(calcDamage(damage, armor, penetration)) *(1 + critPercent);
+		return (int) Math.round(crit);
 	}
 	
 	@RequestMapping(value = "criticalStrikeBySeconds", method = RequestMethod.GET, produces=MediaType.APPLICATION_JSON_VALUE)
 	public @ResponseBody String calcCriticalStrikeBySeconds(@RequestParam("damage") Integer damage, @RequestParam("armor") Integer armor, @RequestParam("penetration") Integer penetration, @RequestParam("critPercent") Double critPercent, @RequestParam("seconds") Integer seconds){
+		
 		return null;
 	}
 
--- src/main/java/br/com/lolcalc/ejbs/interfaces/DamageCalc.java
+++
@@ -1,8 +0,0 @@
-package br.com.lolcalc.ejbs.interfaces;
-
-import javax.ejb.Remote;
-
-@Remote
-public interface DamageCalc {
-	public Integer calcDamage(Integer damage, Integer armor, Integer penetration);
-}
